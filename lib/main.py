d62e422a70cbe0b82f9bb27c0614749e6af309e9
Rowland <rowland.wanderi@student.moringaschool.com>
2023-12-12 16:40:09 +0000

    added a method that creates a new user

---
+++ lib/main.py
@@ -0,0 +1,12 @@
+from models import *
+
+
+#Create a new user with a first name,  and last name
+def add_user(first_name,last_name):
+    new_user = User(
+        first_name=first_name,
+        last_name = last_name
+    )
+    #save it to the database
+    session.add(new_user)
+    session.commit()
